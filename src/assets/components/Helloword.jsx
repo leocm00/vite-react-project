b94fc1c026ea51bd2566b0b594bd96045667cc4a
leocm00 <leocm1010@gmail.com>
2022-08-07 23:31:36 -0500

    first commit

---
+++ src/assets/components/Helloword.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function helloword(){
+    return (
+        <div>Hola Mundo desde React! con ViteJS</div>
+    )
+}
+
+export default helloword;
---
+++ src/assets/components/Vitereact.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Vitereact(){
+    return(
+        <h1>Vite + React!</h1>
+    )
+}
+
+export default Vitereact;
